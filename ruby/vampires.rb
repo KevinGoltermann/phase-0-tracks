e0ab97ac2d418ea4b5b486814bf9993fd7336b29
Kevin Goltermann <kggoltermann@gmail.com>
2016-08-25 08:20:04 -0500

    started module 4.3

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How are you? What year were you born?"
+age = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
