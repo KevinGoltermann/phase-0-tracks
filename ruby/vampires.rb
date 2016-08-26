43716ecdae2d6091da62515f371ebed2825c6aaf
Kevin Goltermann <kggoltermann@gmail.com>
2016-08-26 00:19:00 -0500

    added another release to solo challenge

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,30 +1,38 @@
-puts "What is your name?"
-name = gets.chomp.downcase
+puts "How many employees will be processed"
+total_employees = gets.to_i
 
-puts "How old are you?" 
-age = gets.to_i
+total_employees.times do
 
-puts "What year were you born?"
-year = gets.to_i
+	puts "What is your name?"
+	name = gets.chomp.downcase
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-bread = gets.chomp.downcase
+	puts "How old are you?" 
+	age = gets.to_i
+
+	puts "What year were you born?"
+	year = gets.to_i
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	bread = gets.chomp.downcase
 
 
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp.downcase
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp.downcase
 
 
-actual_age = 2016 - year
+	actual_age = 2016 - year
 
-if age == actual_age && bread == 'yes' || insurance == 'yes'
-  puts "probably not a vampire"
-elsif age != actual_age && bread == 'no' || waivers == 'no'
-  puts "probably a vampire"
-elsif age != actual_age && bread == "no" && insurance == "no"
-  puts "Almost certainly a vampire"
-elsif name == 'drake cula' || name == 'tu fang'
-  puts "Definitely a vampire"
-else
-  puts "results inconclusive"
+
+	if age == actual_age && bread == 'yes' || insurance == 'yes'
+	  puts "probably not a vampire"
+	elsif age != actual_age && bread == 'no' || waivers == 'no'
+	  puts "probably a vampire"
+	elsif age != actual_age && bread == "no" && insurance == "no"
+	  puts "Almost certainly a vampire"
+	elsif name == 'drake cula' || name == 'tu fang'
+	  puts "Definitely a vampire"
+	else
+	  puts "results inconclusive"
+	end
+	
 end
