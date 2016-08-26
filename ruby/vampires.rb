aef71de0b0a3ed3b38dc0d68202bb9db88fd27df
Kevin Goltermann <kggoltermann@gmail.com>
2016-08-25 22:57:32 -0500

    updated solo challenge

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,30 @@
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.downcase
 
-puts "How are you? What year were you born?"
-age = gets.chomp
+puts "How old are you?" 
+age = gets.to_i
+
+puts "What year were you born?"
+year = gets.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-bread = gets.chomp
+bread = gets.chomp.downcase
+
 
 puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
+insurance = gets.chomp.downcase
+
+
+actual_age = 2016 - year
+
+if age == actual_age && bread == 'yes' || insurance == 'yes'
+  puts "probably not a vampire"
+elsif age != actual_age && bread == 'no' || waivers == 'no'
+  puts "probably a vampire"
+elsif age != actual_age && bread == "no" && insurance == "no"
+  puts "Almost certainly a vampire"
+elsif name == 'drake cula' || name == 'tu fang'
+  puts "Definitely a vampire"
+else
+  puts "results inconclusive"
+end
