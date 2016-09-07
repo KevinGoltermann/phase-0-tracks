2f2d17410e7e01e765ef86ee8168ee27cb32aeea
Kevin Goltermann <kggoltermann@gmail.com>
2016-09-06 20:54:06 -0500

    completed 6.1 Introduction to Classes

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Species ------------------
+# Cuteus Puppyus
+
+# Characteristics --------------------
+# Name: varies
+# Color: varies
+# Ear length: long
+# Weight: varies
+# Gets along with other dogs: yes
+
+# Behavior ----------------
+# Playful
+# Likes people
+# Plays fetch
