02a8d7874050a3e4c56309cec52d0d9c2d6689b8
Kevin Goltermann <kggoltermann@gmail.com>
2016-09-12 21:32:25 -0500

    finished 7.1 syntax

---
+++ js/explore.js
@@ -0,0 +1,25 @@
+// Pseudcode
+// 1.) function has to take a string as an input
+// 2.) function then splits up the string into each character
+// 3.) function reverses those characters
+// 4.) function puts the characters back into one string
+
+function reverse(s) {
+  return s.split('').reverse().join('');
+}
+
+console.log(reverse("reverse"))
+
+var stored = reverse("reverse")
+
+console.log(stored)
+
+var number = 1
+
+if (number == 1) {
+	console.log(stored)
+}
+
+
+
+
