3b4ab645760dbee9d736cb9074f71b2f737444f8
Kevin Goltermann <kggoltermann@gmail.com>
2016-09-13 22:18:31 -0500

    finished gps3.1

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,14 +1,17 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Andrew van Beek ].
+# We spent [2] hours on this challenge. 
 
-# EXPLANATION OF require_relative
+# EXPLANATION OF require_relative: require_relative is the command that is responsible for calling another file to load. Require is the long way to do require_relative
 #
 #
 require_relative 'state_data'
 
 class VirusPredictor
+
+
+# initialize method is responsible for creating instances and taking the vatiable given 
 
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
@@ -16,14 +19,20 @@ class VirusPredictor
     @population_density = population_density
   end
 
+
+# Responsible for printing both of the statements together, since they don't make sense if they're alone.
+  
+
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
-  private
+  
+private
 
-  def predicted_deaths(population_density, population, state)
+# Taking the info from the other file, comparing to the if statement, and doing to correspsonding math. Then prints a statement with the state and the answer to the equation.
+  def predicted_deaths #(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +50,8 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# Same thing as method above, except instead of doing math, it adds the correspinding speed to the speed variable. And then prints the statement.
+  def speed_of_spread #(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -58,6 +68,8 @@ class VirusPredictor
       speed += 2.5
     end
 
+    if @population_density
+
     puts " and will spread across the state in #{speed} months.\n\n"
 
   end
@@ -69,19 +81,35 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each do |state, hashes|
+  call = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+  call.virus_effects
+end
+end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+#alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+#alabama.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+#jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+#jersey.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+#california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+#california.virus_effects
+
+#alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+#alaska.virus_effects
 
 
 #=======================================================================
 # Reflection Section
+
+# In the state_data file, the hash uses both keys and symbols. This is because there is a hash imbedded inside of another hash.
+
+# require_relative is the command that is responsible for calling another file to load. Require is the long way to do require_relative
+
+# We used .each to iterate through out hash since we wanted to apply our methods to each state. You can also use .each_value and .each_key
+
+# What stood out to me was that the aruements that were orignally after each method in virus_effects were not even needed. I forgot this was possible because everything is inside the same class, which means it is accessible everywhere in the class.
+
+# I think that the concept of iterating through a hash was more solidified in this GPS, even though I'm not 100# on it yet. Our guide was really helpful at walking us through it and letting us figure it out on our own.
