d4604ebf91c8cce72acbb27e27ca60021ef2084e
Kevin Goltermann <kggoltermann@gmail.com>
2016-09-14 21:38:29 -0500

    finished pairing session

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,11 @@
-/*var colors = ["blue", "orange", "red", "black"];
+var colors = ["blue", "orange", "red", "black"];
 var names = ["Ed", "Carrot", "Captain", "Tom"];
 
 colors.push("green");
 names.push("Sky");
 
-console.log("The horse named" + names[Math.floor(Math.random() * names.length)] + " is " + colors[Math.floor(Math.random() * colors.length)])
-*/
+console.log("The horse named " + names[Math.floor(Math.random() * names.length)] + " is " + colors[Math.floor(Math.random() * colors.length)])
+
 
 function Build(type, make, year, engine) 
 {     console.log("Car specifics:", this)       
